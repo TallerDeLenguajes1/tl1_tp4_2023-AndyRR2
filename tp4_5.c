24ed0ea8e5629f22c07afe673c728b346fa9a1f0
Andy Rodriguez <andyarr222@gmail.com>
2023-04-15 14:04:30 -0300

    subo con buscarporID a buscar-tarea

--- tp4_5.c
+++ tp4_5.c
@@ -11,9 +11,11 @@ struct Tarea{
 void cargarTarea(struct Tarea **tareasP, int num);
 void mostrarTarea(struct Tarea **tareasP, int num);
 void realizarTarea(struct Tarea **tareasP,struct Tarea **tareasR, int num, int num2);
+struct Tarea* buscarTarea(struct Tarea **tareas, int num, int ID);
 
 int main(){
-    int cant, num, num2, estado;
+    int cant, num, num2, estado, ID;
+    struct Tarea *porID=NULL;
     struct Tarea **tareasP=NULL;
     struct Tarea **tareasR=NULL;
     printf("Entre la cantidad de tareas: ");
@@ -66,7 +68,26 @@ int main(){
         }
     }
     printf("\n");
+    
+    printf("Entre un ID a buscar: ");
+    scanf("%d",&ID);
+    fflush(stdin);
+    //porID=(struct Tarea*)malloc(1*sizeof(struct Tarea));
+    porID=buscarTarea(tareasP,cant,ID);
+    if (porID!=NULL){
+        printf("Tarea buscada: \n");
+        printf("ID: %d\n",porID->TareaID);
+        printf("Descripcion: ");
+        puts(porID->Descripcion);
+        printf("Duracion: %d\n",porID->Duracion);
+    }else{
+        printf("Tarea no encontrada\n");
+    }
+    
+    
+    
 
+    free(porID);
     free(tareasP);
     free(tareasR);
     return(0);
@@ -102,4 +123,15 @@ void realizarTarea(struct Tarea **tareasP, struct Tarea **tareasR, int num, int 
    tareasR[num2]=(struct Tarea*)malloc(sizeof(struct Tarea));
    tareasR[num2]=tareasP[num];
    tareasP[num]=NULL;
+}
+
+struct Tarea* buscarTarea(struct Tarea **tareas, int num, int ID){
+   for (int i = 0; i < num; i++){
+    if (tareas[i]!=NULL){
+        if (tareas[i]->TareaID==ID){
+        return(tareas[i]);
+        }
+    }
+   }
+return NULL;
 }
